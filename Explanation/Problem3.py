60226af56fc1bcbad3bcc10f1113825d96517b43
katsuta1104 <evening698564@gmail.com>
2024-11-08 13:52:39 +0900

    Problem3.py
    Problem3の解説

---
+++ Explanation/Problem3.py
@@ -0,0 +1,14 @@
+N=int(input())
+A=list(map(int,input().split()))
+X=int(input())
+ans=0
+
+for i in range(N**2):
+    sum_value=0
+    for j in range(N):
+        if (i>>j)&1:
+            sum_value+=A[j]
+    
+    if sum_value==X:
+        ans+=1
+print(ans)
