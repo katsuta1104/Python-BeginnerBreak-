77a2ad00e665fc119268ac80b5e655a565654447
katsuta1104 <evening698564@gmail.com>
2024-11-08 13:38:49 +0900

    Problem1.py

--- Explanation/Problem1.py
+++ Explanation/Problem1.py
@@ -1,1 +1,12 @@
-解説
+A,B,C,S=map(int,input().split())
+#A,B,C,Sを受け取る
+ans=0
+
+for i in range(1,A+1):
+  for j in range(1,B+1):
+    for k in range(1,C+1):
+      if i+j+k==S:
+        ans+=1
+      #もし、i+j+kがSと等しいならansを1増やす
+
+print(ans)
